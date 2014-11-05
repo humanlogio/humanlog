f5c17a6fb08ebc29d9fd0e3974da98cd011812b4
Antoine Grondin <antoinegrondin@gmail.com>
2014-11-05 15:34:41 -0500

    fix bug, when scanner fails to parse logfmt, print log entry

--- scanner.go
+++ scanner.go
@@ -2,12 +2,15 @@ package humanlog
 
 import (
 	"bufio"
-	"github.com/kr/logfmt"
 	"io"
 	"log"
+
+	"github.com/kr/logfmt"
 )
 
-var eol = [...]byte{'\n'}
+var (
+	eol = [...]byte{'\n'}
+)
 
 // Scanner reads logfmt'd lines from src and prettify them onto dst.
 // If the lines aren't logfmt, it will simply write them out with no
@@ -31,6 +34,8 @@ func Scanner(src io.Reader, dst io.Writer, opts *HandlerOptions) error {
 			err := logfmt.Unmarshal(lineData, &logrusEntry)
 			if err != nil {
 				log.Printf("line %d: parsing logfmt, %v", line, err)
+				lastLogrus = false
+				dst.Write(lineData)
 				continue
 			}
 			dst.Write(logrusEntry.Prettify(opts.SkipUnchanged && lastLogrus))
