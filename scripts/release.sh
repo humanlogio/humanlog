966d709f5de2dd1d04c15244cff2cd10e836e4e9
Antoine Grondin <antoinegrondin@gmail.com>
2016-09-01 10:09:38 -0400

    release bump

--- scripts/release.sh
+++ scripts/release.sh
@@ -20,18 +20,18 @@ fi
 
 set -u -e
 
-rm -rf /tmp/temple_build/
+rm -rf /tmp/humanlog_build/
 
-mkdir -p /tmp/temple_build/linux
-GOOS=linux godep go build -ldflags "-X main.version=$VERSION" -o /tmp/temple_build/linux/temple ../
-pushd /tmp/temple_build/linux/
-tar cvzf /tmp/temple_build/temple_linux.tar.gz temple
+mkdir -p /tmp/humanlog_build/linux
+GOOS=linux godep go build -ldflags "-X main.version=$VERSION" -o /tmp/humanlog_build/linux/humanlog ../
+pushd /tmp/humanlog_build/linux/
+tar cvzf /tmp/humanlog_build/humanlog_linux.tar.gz humanlog
 popd
 
-mkdir -p /tmp/temple_build/darwin
-GOOS=darwin godep go build -ldflags "-X main.version=$VERSION" -o /tmp/temple_build/darwin/temple ../
-pushd /tmp/temple_build/darwin/
-tar cvzf /tmp/temple_build/temple_darwin.tar.gz temple
+mkdir -p /tmp/humanlog_build/darwin
+GOOS=darwin godep go build -ldflags "-X main.version=$VERSION" -o /tmp/humanlog_build/darwin/humanlog ../
+pushd /tmp/humanlog_build/darwin/
+tar cvzf /tmp/humanlog_build/humanlog_darwin.tar.gz humanlog
 popd
 
 temple file < README.tmpl.md > ../README.md -var "version=$VERSION"
@@ -39,8 +39,8 @@ git add ../README.md
 git commit -m 'release bump'
 
 hub release create \
-    -a /tmp/temple_build/temple_linux.tar.gz \
-    -a /tmp/temple_build/temple_darwin.tar.gz \
+    -a /tmp/humanlog_build/humanlog_linux.tar.gz \
+    -a /tmp/humanlog_build/humanlog_darwin.tar.gz \
     $VERSION
 
 git push origin master
