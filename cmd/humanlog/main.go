5827550868217aecbde31ac5b0896aca6d0eb5ab
Antoine Grondin <antoinegrondin@gmail.com>
2017-04-11 01:22:57 +0900

    log to stderr

--- cmd/humanlog/main.go
+++ cmd/humanlog/main.go
@@ -23,7 +23,7 @@ func main() {
 
 	prefix := rgbterm.FgString(app.Name+"> ", 99, 99, 99)
 
-	log.SetOutput(colorable.NewColorableStdout())
+	log.SetOutput(colorable.NewColorableStderr())
 	log.SetFlags(0)
 	log.SetPrefix(prefix)
 	err := app.Run(os.Args)
