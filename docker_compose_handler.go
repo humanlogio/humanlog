071acc362a9b5aa747fdc4d2f43f7c8decfaafa3
Bouke van der Bijl <i@bou.ke>
2020-02-18 14:35:58 +0100

    Add verbose comment explaning the regex

--- docker_compose_handler.go
+++ docker_compose_handler.go
@@ -5,6 +5,12 @@ import (
 )
 
 // dcLogsPrefixRe parses out a prefix like 'web_1 | ' from docker-compose
+// The regex exists of five parts:
+// 1. An optional color terminal escape sequence
+// 2. The name of the service
+// 3. Any number of spaces, and a pipe symbol
+// 4. An optional color reset escape sequence
+// 5. The rest of the line
 var dcLogsPrefixRe = regexp.MustCompile("^(?:\x1b\\[\\d+m)?(?P<service_name>[a-zA-Z0-9._-]+)\\s+\\|(?:\x1b\\[0m)? (?P<rest_of_line>.*)$")
 
 type handler interface {
