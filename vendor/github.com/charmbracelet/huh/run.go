85759839803cf69f66d31e5be6b06544c9b46e20
Antoine Grondin <antoinegrondin@gmail.com>
2024-10-28 23:41:58 +0900

    build(localstorage): pull pro storage layer at build time

--- vendor/github.com/charmbracelet/huh/run.go
+++
@@ -1,8 +0,0 @@
-package huh
-
-// Run runs a single field by wrapping it within a group and a form.
-func Run(field Field) error {
-	group := NewGroup(field)
-	form := NewForm(group).WithShowHelp(false)
-	return form.Run()
-}
