601855c85fe47d28b23ae3c8220a44a0c1659221
Antoine Grondin <antoinegrondin@gmail.com>
2025-02-04 12:33:58 +0900

    fix(service): detach from process on self-update

--- cmd/humanlog/service.go
+++ cmd/humanlog/service.go
@@ -617,7 +617,7 @@ func (hdl *serviceHandler) DoUpdate(ctx context.Context) error {
 		channelName = hdl.config.ExperimentalFeatures.ReleaseChannel
 	}
 	ll.InfoContext(ctx, "starting upgrade in place")
-	if err := selfupdate.UpgradeInPlace(ctx, baseSiteURL, channelName, os.Stdout, os.Stderr, os.Stdin); err != nil {
+	if err := selfupdate.UpgradeInPlace(ctx, baseSiteURL, channelName, nil, nil, nil, true); err != nil {
 		return fmt.Errorf("applying self-update: %v", err)
 	}
 	ll.InfoContext(ctx, "triggering self-shutdown, hoping the service manager will restart us")
