c1d8b6d2394e7ca0c560baea8b2e1ddac2143537
KevRiver <kanggakky1214@gmail.com>
2025-02-24 16:40:01 +0900

    fix(update): install new binary under temp dir and when installation completed, swap binary via os.Rename

--- internal/pkg/selfupdate/update.go
+++ internal/pkg/selfupdate/update.go
@@ -4,8 +4,10 @@ package selfupdate
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
+	"io/fs"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -17,12 +19,6 @@ import (
 )
 
 func UpgradeInPlace(ctx context.Context, curSV semver.Version, baseSiteURL string, channelName *string, stdout, stderr io.Writer, stdin io.Reader, detach bool) error {
-	suffix := fmt.Sprintf("-v%d-%d-%d", curSV.Major, curSV.Minor, curSV.Patch)
-	cur, renamed, err := renameCurrentBinaryWithSuffix(suffix)
-	if err != nil {
-		return fmt.Errorf("renaming current binary: %v", err)
-	}
-
 	shellToUse, ok := os.LookupEnv("SHELL")
 	switchToUse := "-c"
 
@@ -37,11 +33,18 @@ func UpgradeInPlace(ctx context.Context, curSV semver.Version, baseSiteURL strin
 
 	command := updateCommand(baseSiteURL)
 
+	tmp, err := os.MkdirTemp("", "*")
+	if err != nil {
+		return fmt.Errorf("making temporary directory in order to install new binary: %v", err)
+	}
+	defer os.RemoveAll(tmp)
+
 	cmd := exec.Command(shellToUse, switchToUse, command)
 	cmd.Stdout = stdout
 	cmd.Stderr = stderr
 	cmd.Stdin = stdin
 	cmd.Env = append(cmd.Env, "INSIDE_HUMANLOG_SELF_UPDATE=true")
+	cmd.Env = append(cmd.Env, fmt.Sprintf("HUMANLOG_INSTALL=%s", tmp))
 	if channelName != nil {
 		cmd.Env = append(cmd.Env, fmt.Sprintf("HUMANLOG_CHANNEL=%s", *channelName))
 	}
@@ -55,9 +58,22 @@ func UpgradeInPlace(ctx context.Context, curSV semver.Version, baseSiteURL strin
 		return nil
 	} else {
 		if err := cmd.Run(); err != nil {
-			// failed to install new binary, rollback renamed binary's name to previous name
-			os.Rename(renamed, cur)
 			return fmt.Errorf("running update script: %v", err)
+		}
+		currentBinaryPath, err := os.Executable()
+		if err != nil {
+			return fmt.Errorf("getting executable path: %v", err)
+		}
+		newBinaryPath := filepath.Join(tmp, "bin", "humanlog") // command 'install.sh | sh' makes new directory 'bin' under the 'HUMANLOG_INSTALL' path
+		_, err = os.Stat(newBinaryPath)
+		if err != nil {
+			if errors.Is(err, fs.ErrNotExist) {
+				return fmt.Errorf("new binary not exists: %v", err)
+			}
+			return fmt.Errorf("stat file at %s: %v", newBinaryPath, err)
+		}
+		if err := os.Rename(newBinaryPath, currentBinaryPath); err != nil {
+			return fmt.Errorf("replace current binary with new one: %v", err)
 		}
 		return nil
 	}
@@ -93,16 +109,4 @@ func updateCommand(baseSiteURL string) string {
 	} else {
 		return fmt.Sprintf(`curl -sSL "%s/install.sh" | sh`, baseSiteURL)
 	}
-}
-
-func renameCurrentBinaryWithSuffix(suffix string) (string, string, error) {
-	binaryPath, err := os.Executable()
-	if err != nil {
-		return binaryPath, "", err
-	}
-	newBinaryPath := binaryPath + suffix
-	if err := os.Rename(binaryPath, newBinaryPath); err != nil {
-		return binaryPath, newBinaryPath, fmt.Errorf("renaming current binary to %s: %v", newBinaryPath, err)
-	}
-	return binaryPath, newBinaryPath, nil
 }
