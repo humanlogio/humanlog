3aa9f0458d92ac9676edccd5937251516bdffd8f
Antoine Grondin <antoinegrondin@gmail.com>
2025-04-24 18:40:56 +0900

    feat(localhost): show database stats

--- cmd/humanlog/api.go
+++ cmd/humanlog/api.go
@@ -4,12 +4,17 @@ import (
 	"context"
 	"encoding/json"
 	"log/slog"
+	"net"
 	"net/http"
+	"net/url"
 	"os"
+	"strconv"
 
 	"connectrpc.com/connect"
 	featurev1 "github.com/humanlogio/api/go/svc/feature/v1"
 	"github.com/humanlogio/api/go/svc/feature/v1/featurev1connect"
+	localhostv1 "github.com/humanlogio/api/go/svc/localhost/v1"
+	"github.com/humanlogio/api/go/svc/localhost/v1/localhostv1connect"
 	"github.com/humanlogio/humanlog/internal/pkg/config"
 	"github.com/humanlogio/humanlog/internal/pkg/state"
 	"github.com/humanlogio/humanlog/pkg/auth"
@@ -30,6 +35,7 @@ func apiCmd(
 	getAPIUrl func(cctx *cli.Context) string,
 	getBaseSiteURL func(cctx *cli.Context) string,
 	getHTTPClient func(cctx *cli.Context, apiURL string) *http.Client,
+	getLocalhostHTTPClient func(cctx *cli.Context) *http.Client,
 	getConnectOpts func(*cli.Context) []connect.ClientOption,
 ) cli.Command {
 	return cli.Command{
@@ -37,6 +43,16 @@ func apiCmd(
 		Usage:  "Raw interactions with the API",
 		Hidden: true,
 		Subcommands: []cli.Command{
+			apiLocalhost(
+				getCtx,
+				getLogger,
+				getCfg,
+				getState,
+				getTokenSource,
+				getAPIUrl,
+				getLocalhostHTTPClient,
+				getConnectOpts,
+			),
 			apiFeature(
 				getCtx,
 				getLogger,
@@ -47,6 +63,55 @@ func apiCmd(
 				getHTTPClient,
 				getConnectOpts,
 			),
+		},
+	}
+}
+
+func apiLocalhost(
+	getCtx func(cctx *cli.Context) context.Context,
+	getLogger func(cctx *cli.Context) *slog.Logger,
+	getCfg func(cctx *cli.Context) *config.Config,
+	getState func(cctx *cli.Context) *state.State,
+	getTokenSource func(cctx *cli.Context) *auth.UserRefreshableTokenSource,
+	getAPIUrl func(cctx *cli.Context) string,
+	getLocalhostHTTPClient func(cctx *cli.Context) *http.Client,
+	getConnectOpts func(cctx *cli.Context) []connect.ClientOption,
+) cli.Command {
+
+	getLocalhostClient := func(cctx *cli.Context) localhostv1connect.LocalhostServiceClient {
+
+		cfg := getCfg(cctx)
+		port := cfg.GetRuntime().GetExperimentalFeatures().GetServeLocalhost().GetPort()
+
+		localhostAddr := net.JoinHostPort("localhost", strconv.FormatInt(port, 10))
+		addr, err := url.Parse("http://" + localhostAddr)
+		if err != nil {
+			panic(err)
+		}
+		httpClient := getLocalhostHTTPClient(cctx)
+		client := localhostv1connect.NewLocalhostServiceClient(httpClient, addr.String())
+		return client
+	}
+
+	return cli.Command{
+		Name: "localhost",
+		Subcommands: []cli.Command{
+			{
+				Name: "stats",
+				Action: func(cctx *cli.Context) error {
+					ctx := getCtx(cctx)
+					ll := getLogger(cctx)
+					client := getLocalhostClient(cctx)
+					ll.InfoContext(ctx, "looking localhost stats")
+					req := &localhostv1.GetStatsRequest{}
+					res, err := client.GetStats(ctx, connect.NewRequest(req))
+					if err != nil {
+						return err
+					}
+					_, err = os.Stdout.WriteString(protojson.Format(res.Msg))
+					return err
+				},
+			},
 		},
 	}
 }
