56303046347040236813dc9f36f3715e08b28daf
Antoine Grondin <antoinegrondin@gmail.com>
2025-07-25 18:20:23 +0900

    fix: bad reference

--- cmd/humanlog/systray_other.go
+++ cmd/humanlog/systray_other.go
@@ -7,7 +7,6 @@ import (
 	"log/slog"
 
 	typesv1 "github.com/humanlogio/api/go/types/v1"
-	"github.com/humanlogio/humanlog/internal/localalert"
 )
 
 func runSystray(ctx context.Context, ll *slog.Logger, svcHandler *serviceHandler, version *typesv1.Version, baseSiteURL string) error {
@@ -23,7 +22,7 @@ func newSystrayController(ctx context.Context, ll *slog.Logger, client serviceCl
 	return &systrayController{ll: ll}, nil
 }
 
-func (ctrl *systrayController) NotifyAlert(ctx context.Context, ar *typesv1.AlertRule, as localalert.AlertStatus, o *typesv1.Obj) error {
+func (ctrl *systrayController) NotifyAlert(ctx context.Context, ar *typesv1.AlertRule, as *typesv1.AlertState, o *typesv1.Obj) error {
 	ctrl.ll.WarnContext(ctx, "systray: not implemented on this platform: NotifyError")
 	return nil
 }
